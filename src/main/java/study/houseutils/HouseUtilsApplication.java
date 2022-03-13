b1430891288ba2c608789e489ab55c5a19d1c75a
profoundsea25 <profoundsea25@gmail.com>
2022-03-13 21:30:00 +0900

    [DAY 02] 실습 - 01.프로젝트, 2022.03.13

---
+++ src/main/java/study/houseutils/HouseUtilsApplication.java
@@ -0,0 +1,13 @@
+package study.houseutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/houseutils/HouseUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.houseutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
