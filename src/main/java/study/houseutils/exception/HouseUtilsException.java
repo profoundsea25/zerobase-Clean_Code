ceb3bdefe63ccd4b6e1833a22966dada2bc111ed
profoundsea25 <profoundsea25@gmail.com>
2022-03-27 19:02:18 +0900

    [DAY 12] 실습 - 05. 오류처리, 2022.03.27

---
+++ src/main/java/study/houseutils/exception/HouseUtilsException.java
@@ -0,0 +1,17 @@
+package study.houseutils.exception;
+
+public class HouseUtilsException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String message;
+
+    public HouseUtilsException(ErrorCode errorCode) {
+        this(errorCode, errorCode.getMessage());
+    }
+
+    public HouseUtilsException(ErrorCode errorCode, String customMessage) {
+        super(customMessage); // super = RuntimeException
+        this.errorCode = errorCode;
+        this.message = customMessage;
+    }
+
+}
--- src/main/java/study/houseutils/policy/BrokeragePolicyFactory.java
+++ src/main/java/study/houseutils/policy/BrokeragePolicyFactory.java
@@ -1,6 +1,8 @@
 package study.houseutils.policy;
 
 import study.houseutils.constant.ActionType;
+import study.houseutils.exception.ErrorCode;
+import study.houseutils.exception.HouseUtilsException;
 
 public class BrokeragePolicyFactory {
 
@@ -11,7 +13,7 @@ public class BrokeragePolicyFactory {
             case PURCHASE:
                 return new PurchaseBrokeragePolicy();
             default: // ActionType 추가될 것을 고려
-                throw new IllegalArgumentException("해당 actionType에 대한 정책이 존재하지 않습니다.");
+                throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, "해당 actionType에 대한 정책이 존재하지 않습니다.");
         }
     }
 }
